eb5cc009925e94551e8cb66999fad7ae30fca8d2
Adrian Lugo <misteralugo@gmail.com>
2022-03-17 21:45:10 -0500

    fourth test

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ export default function Home({componentOpacity}) {
     return (
         <div className="home-container" style={{opacity: componentOpacity}}>
             <h1>Home Here</h1>
-            <p>Third Test Here</p>
+            <p>Fourth Test Here</p>
         </div>
     )
 }
