6fe0cef610a454c5e8c635926557f37ae1b18cf5
Adrian Lugo <misteralugo@gmail.com>
2022-04-08 23:55:35 -0500

    replaced all icons with svg & increased their size

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,6 @@
 import React from "react"
 import "../styles/Header.scss"
 import { Link } from "react-router-dom"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 export default function Header({setIsToggled, closeCartMenu}) {
 
