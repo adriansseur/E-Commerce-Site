fe48a828673bac7cb404add5d411d1b2a6decfd2
Adrian Lugo <misteralugo@gmail.com>
2022-04-17 16:04:03 -0500

    added toggle cart animation

--- src/App.js
+++ src/App.js
@@ -57,6 +57,7 @@ export default function App() {
     return (
         <div className="app-container">
             <Header
+                isToggled={isToggled}
                 productsItems={productsItems}
                 setIsToggled={setIsToggled}
                 closeCartMenu={closeCartMenu} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,106 @@
 import React from "react"
 import "../styles/Header.scss"
 import { Link } from "react-router-dom"
+import { gsap } from "gsap"
 
-export default function Header({setIsToggled, closeCartMenu, productsItems}) {
+export default function Header({isToggled, setIsToggled, closeCartMenu, productsItems}) {
 
-    // Two handleClick functions to close one component if another opens
-    function handleCartClick() {
-        setIsToggled(prevState => 
-            ({menu: false, cart: !prevState.cart})
-        )
+    const cart = React.useRef()
+    const c = gsap.utils.selector(cart)
+    const shapes = React.useRef()
+    const s = gsap.utils.selector(shapes)
+    const tl = gsap.timeline({defaults: {
+        duration: 0.3
     }
-
-    function handleMenuClick() {
-        setIsToggled(prevState => 
-            ({menu: !prevState.menu, cart: false})
-        )
-    }
-
+    })
+    
     const totalCartItems = productsItems.reduce((total, i) => {
         total = total + i.amountInCart
         return total
     }, 0)
+    
+    function handleToggleMenu() {
+        if (isToggled.cart === true) {
+            handleToggleCart()
+            tl.to(cart.current, {
+                onComplete: () => {
+                    setIsToggled(prevState => 
+                        ({menu: !prevState.menu, cart: false})
+                    )
+            }})
+
+        } else if (isToggled.cart === false) {
+            setIsToggled(prevState => 
+                ({menu: !prevState.menu, cart: prevState.cart})
+            )
+        }
+    }
+
+    function handleToggleCart() {
+        if (isToggled.menu === true) {
+            setIsToggled(prevState => 
+                ({menu: false, cart: prevState.cart}))
+            toggleCart()
+        } else if (isToggled.menu === false) {
+            toggleCart()
+        }
+    }
+
+    function toggleCart() {
+        if (isToggled.cart === false) {
+            gsap.set(shapes.current, {opacity: 0})
+            tl.to(cart.current, {scale: 0.8})
+                .to(cart.current, { scale: 1 })
+            // this condition avoids cart counter when necessary
+            if (totalCartItems !== 0) {
+                tl.to(c(".cart-counter"), {duration: 0.25, scale: 3, y: 5})
+                    .to(c(".cart-counter"), {background: "#e9ebea", color: "black", duration: 0.2}, "<50%")
+            }
+            tl.to(cart.current, {rotate: 10})
+                .to(cart.current, {rotate: -10})
+                .to(shapes.current, {opacity: 1}, "<")
+                .to(s(".circle"), {y:-10, duration: 0.2, rotation: 10}, "<30%")
+                .to(s(".square"), {y:-10, duration: 0.2, rotation: -5}, "<")
+                .to(s(".triangle"), {y:-10, duration: 0.2, rotation: 15}, "<")
+                .to(cart.current, {yPercent: 110})
+                .to(s(".circle"), {y: -5, x: 0, scale: 1.3, rotation: -20}, "<")
+                .to(s(".square"), {y: 0, x: -5, scale: 1.3, rotation: 30}, "<")
+                .to(s(".triangle"), {
+                  y: 5, x: -8, scale: 1.3, rotation: -25, onComplete: () => {
+                    setIsToggled(prevState => 
+                        ({menu: prevState.menu, cart: !prevState.cart})) 
+                  }}, "<")
+            
+        } else if (isToggled.cart === true) {
+            tl.to(shapes.current, {scale: 0.8})
+                .to(shapes.current, {scale: 1})
+                .to(s(".circle"), {y:-10, x: 0, duration: 0.2, rotation: 10, scale: 1})
+                .to(s(".square"), {y:-10, x: 0, duration: 0.2, rotation: -5, scale: 1}, "<")
+                .to(s(".triangle"), {y:-10, x: 0, duration: 0.2, rotation: 15, scale: 1}, "<")
+                .to(cart.current, {yPercent: 0})
+                .to(s(".circle"), {y: 0, rotation: 0}, "<")
+                .to(s(".square"), {y: 0, rotation: 0}, "<")
+                .to(s(".triangle"), {y: -10, rotation: 0}, "<")
+                .to(cart.current, {rotate: 0})
+                .to(s(".triangle"), { y: 0 }, "<")
+            // this conditional block avoids cart counter when necessary
+            // and ensures state is changed after animations end
+            if (totalCartItems !== 0) {
+                tl.to(c(".cart-counter"), {
+                    background: "red", color: "white", scale: 4, y: 0, onComplete: () => {
+                        setIsToggled(prevState => 
+                        ({menu: prevState.menu, cart: !prevState.cart}))
+                    }
+                })
+            } else {
+                tl.to(cart.current, {duration: 0, onComplete: () => {
+                        setIsToggled(prevState => 
+                        ({menu: prevState.menu, cart: !prevState.cart}))
+                    }
+                })
+            }
+        }
+    }
 
     return (
         <div className="header-container">
@@ -37,21 +117,26 @@ export default function Header({setIsToggled, closeCartMenu, productsItems}) {
                     onClick={closeCartMenu}
                 >Products
                 </Link>
-                <div className="cart-wrapper" onClick={handleCartClick}>
-                    {totalCartItems !== 0 && <p className="cart-counter">{totalCartItems}</p>}
-                    <svg id="cart"  height="2rem"enableBackground="new 0 0 60 60" version="1.1" viewBox="0 0 60 60" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg">
-                    <path d="m11.68 13l-0.833-5h-2.99c-0.446-1.72-1.998-3-3.857-3-2.206 0-4 1.794-4 4s1.794 4 4 4c1.859 0 3.411-1.28 3.858-3h1.294l0.5 3h-0.038l5.171 26.016c-2.465 0.188-4.518 2.086-4.76 4.474-0.142 1.405 0.32 2.812 1.268 3.858 0.949 1.049 2.301 1.652 3.707 1.652h2c0 3.309 2.691 6 6 6s6-2.691 6-6h11c0 3.309 2.691 6 6 6s6-2.691 6-6h4c0.553 0 1-0.447 1-1s-0.447-1-1-1h-4.35c-0.826-2.327-3.043-4-5.65-4s-4.824 1.673-5.65 4h-11.7c-0.826-2.327-3.043-4-5.65-4s-4.824 1.673-5.65 4h-2.35c-0.842 0-1.652-0.362-2.224-0.993-0.577-0.639-0.848-1.461-0.761-2.316 0.152-1.509 1.546-2.69 3.173-2.69h0.791 0.039 38.994c2.751-1e-3 4.988-2.238 4.988-4.988v-23.013h-48.32zm-7.68-2c-1.103 0-2-0.897-2-2s0.897-2 2-2 2 0.897 2 2-0.897 2-2 2zm42 34c2.206 0 4 1.794 4 4s-1.794 4-4 4-4-1.794-4-4 1.794-4 4-4zm-23 0c2.206 0 4 1.794 4 4s-1.794 4-4 4-4-1.794-4-4 1.794-4 4-4zm35-8.987c0 1.647-1.34 2.987-2.987 2.987h-38.192l-4.77-24h45.949v21.013z"/>
-                    </svg>
+                <div className="cart-wrapper" onClick={handleToggleCart}>
+                    <div ref={shapes} className="shapes-wrapper">
+                        <div className="circle"></div>
+                        <div className="triangle"></div>
+                        <div className="square"></div>
+                    </div>
+                    <div id="cart" ref={cart}>
+                        {totalCartItems !== 0 && <p className="cart-counter">{totalCartItems}</p>}
+                        <svg height="1.75rem" viewBox="0 0 60 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12 10H58V34H17L12 10Z" fill="#e9ebea"/>
+                        <path d="M11.68 8L10.847 3H7.857C7.411 1.28 5.859 0 4 0C1.794 0 0 1.794 0 4C0 6.206 1.794 8 4 8C5.859 8 7.411 6.72 7.858 5H9.152L9.652 8H9.614L14.785 34.016C12.32 34.204 10.267 36.102 10.025 38.49C9.883 39.895 10.345 41.302 11.293 42.348C12.242 43.397 13.594 44 15 44H17C17 47.309 19.691 50 23 50C26.309 50 29 47.309 29 44H40C40 47.309 42.691 50 46 50C49.309 50 52 47.309 52 44H56C56.553 44 57 43.553 57 43C57 42.447 56.553 42 56 42H51.65C50.824 39.673 48.607 38 46 38C43.393 38 41.176 39.673 40.35 42H28.65C27.824 39.673 25.607 38 23 38C20.393 38 18.176 39.673 17.35 42H15C14.158 42 13.348 41.638 12.776 41.007C12.199 40.368 11.928 39.546 12.015 38.691C12.167 37.182 13.561 36.001 15.188 36.001H55.012C57.763 36 60 33.763 60 31.013V8H11.68ZM4 6C2.897 6 2 5.103 2 4C2 2.897 2.897 2 4 2C5.103 2 6 2.897 6 4C6 5.103 5.103 6 4 6ZM46 40C48.206 40 50 41.794 50 44C50 46.206 48.206 48 46 48C43.794 48 42 46.206 42 44C42 41.794 43.794 40 46 40ZM23 40C25.206 40 27 41.794 27 44C27 46.206 25.206 48 23 48C20.794 48 19 46.206 19 44C19 41.794 20.794 40 23 40ZM58 31.013C58 32.66 56.66 34 55.013 34H16.821L12.051 10H58V31.013Z" fill="black"/>
+                        </svg>
+                    </div>
                 </div>
 
-                {/* <FontAwesomeIcon id="cart" icon={faCartShopping} onClick={handleCartClick}/> */}
-                <svg id="menu" onClick={handleMenuClick} height="1.5rem" enableBackground="new 0 0 150 150" version="1.1" viewBox="0 0 150 150" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg">
+                <svg id="menu" onClick={handleToggleMenu} height="1.5rem" enableBackground="new 0 0 150 150" version="1.1" viewBox="0 0 150 150" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg">
                     <path d="M15,30h120c8.284,0,15-6.716,15-15s-6.716-15-15-15H15C6.716,0,0,6.716,0,15S6.716,30,15,30z"/>
                     <path d="M135,60H15C6.716,60,0,66.716,0,75s6.716,15,15,15h120c8.284,0,15-6.716,15-15S143.284,60,135,60z"/>
                     <path d="m135 120h-120c-8.284 0-15 6.716-15 15s6.716 15 15 15h120c8.284 0 15-6.716 15-15s-6.716-15-15-15z"/>
                 </svg>
-
-                {/* <FontAwesomeIcon id="menu" icon={faBars} onClick={handleMenuClick}/> */}
             </div>
         </div>
     )
